5c4bbb9da60ab4142d0014eef1321ef6c9de93cb
Dennis290699 <trujilloadrian29@gmail.com>
2025-11-26 19:43:59 -0500

    Taller_14
    Directivas en Vue

--- src/components/contador.vue
+++ src/components/contador.vue
@@ -1,16 +1,98 @@
 <template>
-  <div class="">
-    <h1>Universidad Central del Ecuador</h1>
-    <h2>Carrera de computación</h2>
+  <div>
+    <h1>{{ tit }}</h1>
+    <!-- Usamos las variables reactivas -->
+    <p>{{ numero }} <sup>2</sup> = {{ calcularCuadrado }} </p>
+
+    <p>{{ numero }} <sup>2</sup> = {{ calcularCuadrado }} </p>
+
+    <p>{{ numero }} <sup>2</sup> = {{ calcularCuadrado }} </p>
+
+    <p>{{ numero }} <sup>2</sup> = {{ calcularCuadrado }} </p>
+
+    <p>{{ numero }} <sup>3</sup> = {{ calcularCubo }} </p>
+
+    <p>{{ numero }} <sup>3</sup> = {{ obtenerCubo() }} </p>
+
+    <section>
+      <button v-on:click="incrementar()">Incrementar +1</button>
+      <button v-on:click="decrementar()">Decrementar -1</button>
+      <button v-show="mostrar">Mostrar/ocultar</button>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  // declaracion de propiedades reactivas
+  data() {
+    return {
+      // Variable reactiva
+      numero: this.inicio,
+      titulo: "Contador"
+    }
+  },
+  // declaracion de metodos reactivos
+  methods: {
+    obtenerCuadrado() {
+      console.log("entro a obtenerCuadrado");
+      // cuando se usa una propiedad reactiva dentro de un metodo se usa this
+      return this.numero * this.numero;
+    },
+    obtenerCubo() {
+      console.log("entro a obtenerCubo");
+      // cuando se usa una propiedad reactiva dentro de un metodo se usa this
+      return this.numero * this.numero * this.numero;
+    },
+    incrementar() {
+      this.numero++;
+    },
+    decrementar() {
+      this.numero--;
+    }
+  },
+  // Propiedades computadas
+  // se ejecutan solo si cambia el resultado de la ejecucion de su cuerpo caso contrario no se ejecutan
+  computed: {
+    calcularCuadrado() {
+      console.log("entro a calcularCuadrado");
+      return this.numero * this.numero;
+    },
+    calcularCubo() {
+      console.log("entro a calcularCubo");
+      return this.numero * this.numero * this.numero;
+    }
+  },
+  // props: ['tit', 'inicio', 'mostrar'] // Declaracion Basica de props
+  props: {
+    tit: String,
+    inicio: {
+      type: Number,
+      default: 99,
+      validator: function (value) {
+        return value >= 10;
+      }
+    },
+    mostrar: Boolean
+  }
+  // String, Number, Boolean, Array, Object, Date, function, BigInt
+};
 </script>
 
 <style>
+section {
+  display: flex;
+  justify-content: center;
+  /* flex-direction: column; */
+}
 
+button {
+  margin: 15px;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  background-color: #42b983;
+  color: white;
+}
 </style>
--- src/views/contadorView.vue
+++ src/views/contadorView.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <h1>Entrando desde la vista</h1>
-    <contador/>
-    <contador/>
+    <!-- Usamos solo cuando se espera un numero el v-bind -->
+    <contador tit="Contador1" v-bind:inicio="20" v-bind:mostrar="true" /> 
+    <!-- Usamos cuando se espera un string el : -->
+    <contador tit="Contador2" :inicio="4" :mostrar="false"/>
   </div>
 </template>
 
 <script>
+import Contador from "../components/contador.vue";
 import contador from "../components/contador.vue"
 
 export default {
